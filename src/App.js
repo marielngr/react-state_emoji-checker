c3bfe0e605b4147b99fc79684d996565690fd299
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-19 21:25:22 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -1,56 +1,47 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let code = "?";
+  const [code, setCode] = useState("");
 
   const validCode = "🐡🐠🐋";
+
+  function handleClick(event) {
+    setCode(code + event.target.textContent);
+  }
 
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Pufferfish">
             🐡
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Whale">
             🐋
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClick}>
           <span role="img" aria-label="Clownfish">
             🐠
           </span>
         </button>{" "}
       </div>
-
       <button
         type="button"
         onClick={() => {
+          setCode("");
           console.log("Reset Code!");
         }}
       >
         Reset
       </button>
-
       {code === validCode && <p>Valid code!</p>}
+      <br />
+      aktuell eingegeben: {code}
     </div>
   );
 }
